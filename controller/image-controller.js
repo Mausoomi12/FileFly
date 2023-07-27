a2fa0a97a433077e82efd987e4a16203149f3b05
Mausoomi Roy <mausoomiroy68613@gmail.com>
2023-07-27 15:12:50 +0530

    deploy

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -12,7 +12,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `https://filefly-be.onrender.com/file/${file._id}`});
+        response.status(200).json({ path: `https://filefly.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
